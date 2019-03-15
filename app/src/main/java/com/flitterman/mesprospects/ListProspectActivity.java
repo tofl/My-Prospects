95a0b5c6f6b86b05df7549060a0665f1d0cf53a1
Tom F <thomas.flitterman@ynov.com>
2019-03-15 16:14:15 +0100

    Retrieve prospects

--- app/src/main/java/com/flitterman/mesprospects/ListProspectActivity.java
+++ app/src/main/java/com/flitterman/mesprospects/ListProspectActivity.java
@@ -2,12 +2,49 @@ package com.flitterman.mesprospects;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ListProspectActivity extends AppCompatActivity {
+
+    private String url = "http://dev.audreybron.fr/flux/flux_prospects.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_prospect);
+
+        JsonArrayRequest request = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+
+            @Override
+            public void onResponse(JSONArray response) {
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject prospect = response.getJSONObject(i);
+                        Log.i("HeyOh", prospect.getString("Nom"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
     }
 }
