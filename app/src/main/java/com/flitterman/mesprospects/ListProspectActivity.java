1c9928a22659f08641bb401f02c8d5e4f2c1c35a
Tom F <thomas.flitterman@ynov.com>
2019-03-15 17:36:32 +0100

    Display list of prospects

--- app/src/main/java/com/flitterman/mesprospects/ListProspectActivity.java
+++ app/src/main/java/com/flitterman/mesprospects/ListProspectActivity.java
@@ -2,8 +2,13 @@ package com.flitterman.mesprospects;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
-
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.view.ViewGroup.LayoutParams;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -15,14 +20,22 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListProspectActivity extends AppCompatActivity {
 
     private String url = "http://dev.audreybron.fr/flux/flux_prospects.json";
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_prospect);
+
+        listView = findViewById(R.id.list_prospects);
+
+        final List<String> prospects = new ArrayList<String>();
 
         JsonArrayRequest request = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
 
@@ -32,10 +45,13 @@ public class ListProspectActivity extends AppCompatActivity {
                     try {
                         JSONObject prospect = response.getJSONObject(i);
                         Log.i("HeyOh", prospect.getString("Nom"));
+                        prospects.add(i, prospect.getString("Prénom") + " " + prospect.getString("Nom"));
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
                 }
+                ArrayAdapter<String>  arrayAdapter = new ArrayAdapter<String>(ListProspectActivity.this, android.R.layout.simple_list_item_1, prospects);
+                listView.setAdapter(arrayAdapter);
             }
         }, new Response.ErrorListener() {
             @Override
