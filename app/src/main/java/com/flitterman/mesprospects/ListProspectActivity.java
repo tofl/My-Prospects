b434c81448ce5678dbaf16e0aec075de2d8b6b51
Tom F <thomas.flitterman@ynov.com>
2019-04-03 13:00:23 +0200

    Display list of prospects

--- app/src/main/java/com/flitterman/mesprospects/ListProspectActivity.java
+++ app/src/main/java/com/flitterman/mesprospects/ListProspectActivity.java
@@ -1,12 +1,15 @@
 package com.flitterman.mesprospects;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Layout;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
@@ -36,6 +39,7 @@ public class ListProspectActivity extends AppCompatActivity {
 
     private String url = "http://dev.audreybron.fr/flux/flux_prospects.json";
     private ListView listView;
+    private ProspectsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +57,7 @@ public class ListProspectActivity extends AppCompatActivity {
 
         final List<String> prospects = new ArrayList<String>();
 
+        /*
         JsonArrayRequest request = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
 
             @Override
@@ -77,77 +82,112 @@ public class ListProspectActivity extends AppCompatActivity {
 
         RequestQueue queue = Volley.newRequestQueue(this);
         queue.add(request);
+        */
 
         new GetProspects().execute(url);
+
     }
-}
 
-
-class GetProspects extends AsyncTask<String, String, Void> {
-
-    @Override
-    protected Void doInBackground(String... urls) {
-        HttpHandler sh = new HttpHandler();
-        String jsonString = sh.makeServiceCall(urls[0]);
-        Log.e("HeyOh", "Json string : " + jsonString);
+    public class GetProspects extends AsyncTask<String, String, Void> {
 
         ArrayList<Prospect> listProspect = new ArrayList<Prospect>();
 
+        @Override
+        protected Void doInBackground(String... urls) {
+            HttpHandler sh = new HttpHandler();
+            String jsonString = sh.makeServiceCall(urls[0]);
+            //Log.e("HeyOh", "Json string : " + jsonString);
 
-        if (jsonString != null) {
-            try {
-                JSONArray jsonObj = new JSONArray(jsonString);
 
-                for (int i = 0; i < jsonObj.length(); i++) {
-                    JSONObject c = jsonObj.getJSONObject(i);
+            if (jsonString != null) {
+                try {
+                    JSONArray jsonObj = new JSONArray(jsonString);
 
-                    String lastname = c.getString("Nom");
-                    String firstname = c.getString("Prénom");
-                    String email = c.getString("Mail");
-                    String addr1 = c.getString("Adresse 1");
-                    String addr2 = c.getString("Addresse 2");
-                    String postCode = c.getString("Code Postal");
-                    String town = c.getString("Ville");
-                    String phone = c.getString("Téléphone");
-                    String mail = c.getString("Mail");
-                    String company = c.getString("Nom Entreprise");
+                    for (int i = 0; i < jsonObj.length(); i++) {
+                        JSONObject c = jsonObj.getJSONObject(i);
 
-                    Prospect prospect = new Prospect();
-                    prospect.setName(lastname);
-                    prospect.setFirstName(firstname);
-                    prospect.setMail(email);
-                    prospect.setAddr1(addr1);
-                    prospect.setAddr2(addr2);
-                    prospect.setPostCode(postCode);
-                    prospect.setTown(town);
-                    prospect.setTelephone(phone);
-                    prospect.setMail(mail);
-                    prospect.setCompanyName(company);
+                        String lastname = c.getString("Nom");
+                        String firstname = c.getString("Prénom");
+                        String email = c.getString("Mail");
+                        String addr1 = c.getString("Adresse 1");
+                        String addr2 = c.getString("Adresse 2");
+                        String postCode = c.getString("Code Postal");
+                        String town = c.getString("Ville");
+                        String phone = c.getString("Téléphone");
+                        String mail = c.getString("Mail");
+                        String company = c.getString("Nom Entreprise");
 
-                    listProspect.add(prospect);
+                        Prospect prospect = new Prospect();
+                        prospect.setName(lastname);
+                        prospect.setFirstName(firstname);
+                        prospect.setMail(email);
+                        prospect.setAddr1(addr1);
+                        prospect.setAddr2(addr2);
+                        prospect.setPostCode(postCode);
+                        prospect.setTown(town);
+                        prospect.setTelephone(phone);
+                        prospect.setMail(mail);
+                        prospect.setCompanyName(company);
+
+                        //Log.e("Prospects", firstname + " " + lastname);
+
+                        listProspect.add(prospect);
+                    }
+                } catch (Exception e) {
+                    Log.e("Ynov", "There was an error : " + e.getMessage());
                 }
-            } catch (Exception e) {
-                Log.e("Ynov", "There was an error : " + e.getMessage());
+            } else {
+                Log.e("Ynov", "La connexion au serveur n'a pas pu être établie");
             }
-        } else {
-            Log.e("Ynov", "La connexion au serveur n'a pas pu être établie");
+
+            return null;
         }
 
-        return null;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+            // Update the list :
+            adapter = new ProspectsAdapter(ListProspectActivity.this, listProspect);
+            adapter.addAll(listProspect);
+            listView.setAdapter(adapter);
+            //adapter.notifyDataSetChanged();
+        }
+
     }
 
-    @Override
-    protected void onPreExecute() {
-        super.onPreExecute();
-    }
+    public class ProspectsAdapter extends ArrayAdapter<Prospect> {
+        public ProspectsAdapter(Context context, List<Prospect> prospects) {
+            super(context, 0, prospects);
+        }
 
-    @Override
-    protected void onPostExecute(Void result) {
-        super.onPostExecute(result);
-        Prospect test = new Prospect();
-        test.setMail("tom.flitterman@hotmail.fr");
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // Get the data item for this position
+            Prospect prospect = getItem(position);
 
-        Log.e("HeyOh", test.getMail());
+            // Check if an existing view is being reused, otherwise inflate the view
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.row_layout, parent, false);
+            }
+
+            TextView name = (TextView) convertView.findViewById(R.id.name);
+
+            // Populate the data into the template view using the data object
+            name.setText(prospect.getName());
+
+            // Return the completed view to render on screen
+            return convertView;
+        }
+
+        public void destroy(View v) {
+            finish();
+        }
     }
 
 }
